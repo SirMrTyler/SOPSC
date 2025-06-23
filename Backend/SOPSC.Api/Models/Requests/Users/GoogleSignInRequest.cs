a66d7170f0ef8f84ad5ed3c272a20319948de92c
SirMrTyler <sirmrtyler.business@gmail.com>
2025-06-23 14:33:52 -0700

    FIXED (API/FRONTEND LINK) / ISSUE:
    New Error:
    
    Google sign-in failed. The endpoint 'connectionsAddress' is offline.'
    
    FIX ATTEMPT:
    
    Changed DeviceId to nullable value as the fr
    ont end does not need to generate the device ID. It's handled in my API

--- Backend/SOPSC.Api/Models/Requests/Users/GoogleSignInRequest.cs
+++ Backend/SOPSC.Api/Models/Requests/Users/GoogleSignInRequest.cs
@@ -8,6 +8,6 @@ namespace SOPSC.Api.Models.Requests.Users
         public string IdToken { get; set; }
 
         // Optional: if app wants to pass deviceId (else we will generate one)
-        public string DeviceId { get; set; }
+        public string? DeviceId { get; set; }
     }
 }
