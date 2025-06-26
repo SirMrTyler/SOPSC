7efb084de982d826b925fd60b9bb7d8a20135dd3
SirMrTyler <sirmrtyler.business@gmail.com>
2025-06-26 11:42:36 -0700

    UPDATE(REFACTOR->AuthScreen.tsx, Login.tsx, LandingPage.tsx)ext.

--- Frontend/sopsc-mobile-app/App.tsx
+++ Frontend/sopsc-mobile-app/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import AuthScreen from './components/LogIn';
+import AuthScreen from './components/Login';
 import LandingPage from './components/LandingPage';
 
 export type RootStackParamList = {
