f30ad26e257a581dd893c4e60795745b5a8b6580
SirMrTyler <sirmrtyler.business@gmail.com>
2025-06-29 11:36:50 -0700

    ISSUE:ext.

--- Frontend/sopsc-mobile-app/src/services/messageService.js
+++ Frontend/sopsc-mobile-app/src/services/messageService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import * as helper from './serviceHelpers';
 
-const endpoint = `${process.env.EXPO_PUBLIC_API_URL}messages}`;
+const endpoint = `${process.env.EXPO_PUBLIC_API_URL}messages`;
 
-const getAll = () => {
-    const token = helper.getToken();
-    const deviceId = helper.getDeviceId();
+const getAll = async () => {
+    const token = await helper.getToken();
+    const deviceId = await helper.getDeviceId();
     const config = {
         method: 'GET',
         url: endpoint,
