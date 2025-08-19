51c18f981713dda4f94a8cc01fd5745d30bac8ae
SirMrTyler <sirmrtyler.business@gmail.com>
2025-08-19 11:18:10 -0700

    Changed all .SQL files file type to .txt for calrity/readability.

--- SQL/Messages/GroupChats/GroupChatMembers_TABLE.sql
+++
@@ -1,9 +0,0 @@
-BEGIN
-    CREATE TABLE dbo.GroupChatMembers(
-        GroupChatId INT NOT NULL,
-        UserId INT NOT NULL,
-        JoinedTimestamp DATETIME2(7) NOT NULL DEFAULT SYSDATETIME(),
-        LastReadTimestamp DATETIME2(7) NULL,
-        CONSTRAINT PK_GroupChatMembers PRIMARY KEY (GroupChatId, UserId)
-    );
-END
