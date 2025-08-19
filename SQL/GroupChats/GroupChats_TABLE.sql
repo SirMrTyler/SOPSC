4c1e11a2a4de51ed3fb19bdb59f254074e8a38a9
SirMrTyler <sirmrtyler.business@gmail.com>
2025-08-19 11:10:01 -0700

    Added .txt SQL tables for clarity

--- SQL/GroupChats/GroupChats_TABLE.sql
+++
@@ -1,8 +0,0 @@
-BEGIN
-    CREATE TABLE dbo.GroupChats(
-        GroupChatId INT IDENTITY(1,1) PRIMARY KEY,
-        Name NVARCHAR(100) NOT NULL,
-        CreatorId INT NOT NULL,
-        CreatedTimestamp DATETIME2(7) NOT NULL DEFAULT SYSDATETIME()
-    );
-END
